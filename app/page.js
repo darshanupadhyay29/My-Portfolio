235274be3e1f58eb8ed9615dec0f59bd451bd05b
darshanupadhyay29 <119104916+darshanupadhyay29@users.noreply.github.com>
2025-01-15 19:47:04 +0530

    Deploying portfolio

--- app/page.js
+++ app/page.js
@@ -1,100 +1,349 @@
-import Image from "next/image";
+"use client"
+
+import  Head  from "next/head";
+import { motion } from "framer-motion";
+import Link from "next/link";
+import { Link as ScrollLink } from "react-scroll";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    
+    const skills = ["/express.png", "/firebase.png", "/git.png", "/javascript.png", "/mongoDb.png", "/nextjs.png", "/node.png", "/postgres.png", "/react.png", "/reactNative.png", "/tailwind.png", "/typescript.png"];
+    
+ const containerVariants = {
+   hidden: { opacity: 0 },
+   visible: {
+     opacity: 1,
+     transition: {
+       staggerChildren: 0.2, // Delay between each image animation
+     },
+   },
+ };
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+ const floatingEffect = {
+   hidden: { opacity: 0, scale: 0.8 },
+   visible: { opacity: 1, scale: 1 },
+   hover: { scale: 1.2, rotate: 10 }, // Scale up and rotate slightly on hover
+   floating: {
+     y: [0, -10, 0, 10, 0], // Move up and down
+     transition: {
+       duration: 4, // Duration of the floating cycle
+       repeat: Infinity, // Repeat endlessly
+       ease: "easeInOut",
+     },
+   },
+ };
+    
+  return (
+    <div className="bg-gray-900 text-white min-h-screen font-sans">
+      <Head>
+        <title>Portfolio</title>
+        <meta name="description" content="My personal portfolio" />
+      </Head>
+
+      {/* Navbar */}
+      <nav className="p-5 bg-gray-800 fixed right-0">
+        <ul className="flex justify-end gap-4">
+          <li>
+            <ScrollLink
+              to="about" // ID of the target section
+              smooth={true}
+              duration={800} // Duration of the scroll (ms)
+              offset={-80} // Offset to account for fixed navbar height
+              className="hover:text-gray-400 cursor-pointer"
+            >
+              About
+            </ScrollLink>
+          </li>
+          <li>
+            <ScrollLink
+              to="skills"
+              smooth={true}
+              duration={800}
+              offset={-80}
+              className="hover:text-gray-400 cursor-pointer"
+            >
+              Skills
+            </ScrollLink>
+          </li>
+          <li>
+            <ScrollLink
+              to="experience"
+              smooth={true}
+              duration={800}
+              offset={-80}
+              className="hover:text-gray-400 cursor-pointer"
+            >
+              Experience
+            </ScrollLink>
+          </li>
+          <li>
+            <ScrollLink
+              to="projects"
+              smooth={true}
+              duration={800}
+              offset={-80}
+              className="hover:text-gray-400 cursor-pointer"
+            >
+              Projects
+            </ScrollLink>
+          </li>
+        </ul>
+      </nav>
+
+      {/* Hero Section */}
+      <header className="flex flex-col items-center justify-center py-20 text-center">
+        <motion.h1
+          className="text-5xl font-bold mb-5 bg-clip-text text-transparent bg-gradient-to-r from-red-500 via-blue-500 to-red-500 animate-gradient-text pb-3"
+          initial={{ opacity: 0, y: 0 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+        >
+          Darshan Upadhyay
+        </motion.h1>
+        <motion.h1
+          className="text-5xl font-bold mb-5"
+          initial={{ opacity: 0, y: -50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+        >
+          Full-stack developer
+        </motion.h1>
+        <motion.p
+          className="text-lg text-gray-400"
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1, delay: 0.5 }}
+        >
+          crafting seamless experiences with expertise in both frontend and
+          backend development.
+        </motion.p>
+      </header>
+
+      {/* Links Section */}
+      <section className="flex justify-center gap-6 py-3">
+        <Link
+          href="https://www.linkedin.com/in/darshan-upadhyay-664176247"
+          target="_blank"
+          className="hover:text-gray-400"
+        >
+          <img src="/linkedIn.png" alt="LinkedIn" height={40} width={40}></img>
+        </Link>
+        <Link
+          href="https://github.com/darshanupadhyay29"
+          target="_blank"
+          className="hover:text-gray-400"
+        >
+          <img src="/github2.png" alt="GitHub" height={40} width={40}></img>
+        </Link>
+        <Link
+          href="https://x.com/DARSHANUPA95530"
+          target="_blank"
+          className="hover:text-gray-400"
+        >
+          <img src="/x.png" alt="X" height={40} width={40}></img>
+        </Link>
+        <Link
+          href="https://leetcode.com/u/darshanupadhyay29"
+          target="_blank"
+          className="hover:text-gray-400"
+        >
+          <img src="/leetcode.png" alt="Leetcode" height={40} width={40}></img>
+        </Link>
+        <Link
+          href="mailto:dbdarshanupadhyay@gmail.com"
+          className="hover:text-gray-400"
+        >
+          <img src="/gmail.png" alt="Gmail" height={40} width={40}></img>
+        </Link>
+      </section>
+
+      {/* About Me Section */}
+      <section id="about" className="p-10 text-center bg-gray-800">
+        <motion.h2
+          className="text-3xl font-bold mb-4"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1 }}
+        >
+          About Me
+        </motion.h2>
+        <img
+          src="/profilepic.png"
+          alt="My Picture"
+          className="w-40 h-40 mb-5 rounded-full mx-auto mt-5"
+        />
+        <p className="text-gray-400">
+          hey! I'm Darshan. I make websites and apps. I work with technologies
+          like react,node.js,next.js.
+          <br />
+          Education : B-tech in Computer Science from IIST College, RGPV
+          university(2021-2025) with CGPA : 7.4/10.
+          <br />
+          I'm always open to new opportunities and collaborations. Feel free to
+          reach out!
+        </p>
+      </section>
+
+      {/* Skills Section */}
+      <section id="skills" className="p-10 text-center">
+        <motion.h2
+          className="text-3xl font-bold mb-4"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1 }}
+        >
+          Technologies
+        </motion.h2>
+        <motion.div
+          initial="hidden"
+          animate="visible"
+          variants={containerVariants}
+          className="grid gap-8 grid-cols-2 sm:grid-cols-4"
+        >
+          {skills.map((i, index) => (
+            <motion.img
+              key={index}
+              src={i}
+              alt={`skill-${index}`}
+              className="w-20 h-20 mx-auto"
+              variants={floatingEffect}
+              initial="visible"
+              animate="floating" // Apply floating effect
+              whileHover="hover" // Apply hover effect
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          ))}
+        </motion.div>
+      </section>
+
+      {/* Experience Section */}
+      <section id="experience" className="p-10 text-center bg-gray-800">
+        <motion.h2
+          className="text-3xl font-bold mb-4"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1 }}
+        >
+          Experience
+        </motion.h2>
+        <div className="grid gap-4">
+          <div className="grid gap-4">
+            <h4>
+              Full Stack Development Intern -{" "}
+              <b>Developer Bazaar Technologies</b> (July'2024 - Oct'2024)
+            </h4>
+          </div>
+          <div className="w-full flex justify-center">
+            <p className="text-gray-400 text-center w-[300px] leading-relaxed">
+              Engineered and maintained a scalable job portal platform using
+              React.js,Node.js,Tailwind CSS,Firebase;optimized database queries
+              and enhanced overall system performance.Integrated gridlines api
+              for PAN card verification.Integrated payment gateway using
+              razorpay.
+            </p>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+      </section>
+
+      {/* Projects Section */}
+      <section id="projects" className="p-5 text-center">
+        <motion.h2
+          className="text-3xl font-bold mb-4"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1 }}
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+          Projects
+        </motion.h2>
+
+        <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 ">
+          {/* project-1 Socio-commerce */}
+          <div className="flex flex-col items-center">
+            <img
+              height={400}
+              width={400}
+              src="/socio-commerce.png"
+              alt="Socio-commerce image"
+              className="transition-transform duration-300 hover:scale-110 p-4"
+            />
+            <div className="flex items-center space-x-2">
+              <text className="font-bold text-lg">Soci-O-Commerce</text>|{" "}
+              <Link
+                href="https://github.com/darshanupadhyay29/socio_commerce"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-purple-600 transition-transform duration-300 hover:text-white"
+              >
+                GitHub
+              </Link>
+              |{" "}
+              <Link
+                className="text-purple-600 transition-transform duration-300 hover:text-white"
+                href="https://socio-commerce.onrender.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Live Site
+              </Link>
+            </div>
+
+            <p className="text-gray-400 text-center w-[300px]  leading-relaxed mt-4">
+              Socio-O-Commerce is a social media platform with an integrated
+              marketplace section for selling and buying products as well.
+            </p>
+            <div className="flex gap-6">
+              <img src="/react.png" alt="react" height={20} width={30}></img>
+              <img src="/node.png" alt="react" height={20} width={30}></img>
+              <img src="/mongoDb.png" alt="react" height={20} width={30}></img>
+              <img src="/express.png" alt="react" height={20} width={30}></img>
+            </div>
+          </div>
+          {/* Project-2 Healthy-wealth */}
+          <div className="flex flex-col items-center">
+            <img
+              height={400}
+              width={400}
+              src="/hw.png"
+              alt="Healthy Wealth image"
+              className="transition-transform duration-300 hover:scale-110 p-4"
+            />
+            <div className="flex items-center space-x-2">
+              <text className="font-bold text-lg">Healthy Wealth</text>|{" "}
+              <Link
+                href="https://github.com/darshanupadhyay29/Healthy-Wealth"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-purple-600 transition-transform duration-300 hover:text-white"
+              >
+                GitHub
+              </Link>
+              |{" "}
+              <Link
+                className="text-purple-600 transition-transform duration-300 hover:text-white"
+                href="https://healthy-wealth.vercel.app"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Live Site
+              </Link>
+            </div>
+            <p className="text-gray-400 text-center w-[300px] leading-relaxed mt-4">
+              Healthy Wealth is a web application to Keep track of your finances
+              with help of AI.
+            </p>
+            <div className="flex gap-6 p-4">
+              <img src="/nextjs.png" alt="react" height={20} width={30}></img>
+              <img src="/tailwind.png" alt="react" height={20} width={30}></img>
+              <img src="/postgres.png" alt="react" height={20} width={30}></img>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* Footer */}
+      <footer className="p-5 bg-gray-800 text-center text-gray-400">
+        Indore - Madhya Pradesh (India)
+        <br />
+        Gmail - dbdarshanupadhyay@gmail.com
       </footer>
     </div>
   );
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        "gradient-text": "gradientText 3s ease infinite",
+      },
+      keyframes: {
+        gradientText: {
+          "0%, 100%": { backgroundPosition: "0% 50%" },
+          "50%": { backgroundPosition: "100% 50%" },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
+ 
+
